6cff351899d01cec803dd90ae1040129ab453a76
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-01-25 10:37:45 +0100

    QuizBrain class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for index in question_data:
     new_q = Question(index["text"], index["answer"])
     question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
