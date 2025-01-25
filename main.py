db15b40bc7e49985cae69851b3c31a84f0f12e1b
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-01-25 11:28:46 +0100

    Full Quiz Game

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for index in question_data:
-    new_q = Question(index["text"], index["answer"])
+    new_q = Question(index["question"], index["correct_answer"])
     question_bank.append(new_q)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.user_score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,30 @@
 class QuizBrain:
+    #Attributes:
     def __init__(self, q_list):
         self.question_number = 0
+        self.user_score = 0
         self.question_list = q_list
+
+    #Methods:
+    def still_has_questions(self):
+        if self.question_number <= len(self.question_list) - 1:
+            return True
+        else:
+            return False
+        #Simplified: return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
-#Q.1: A slug's blood is green. (True/False)?:
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.user_score += 1
+        else:
+            print("That's wrong.")
+        print(f"Correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.user_score}/{self.question_number}")
+        print("\n")
